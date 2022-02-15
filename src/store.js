b916e3114854fdf9d3643272003c242b1d124953
vicky4github <vy4693@gmail.com>
2022-02-15 22:37:04 +0530

    Removed logger for redux for production.

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ import rootReducer from './reducers';
 
 const initialState = {};
 const loggerMiddleware = createLogger();
-const middleware = [thunk, loggerMiddleware];
+const middleware = [thunk]; // add loggerMiddleware with thunk at index 1 to start seeing redux logs
 
 const store = createStore(
   rootReducer,
