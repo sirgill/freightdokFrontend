38cc3103acaa80a391f069961052351ffa219ba5
jugalTiwari <times.jugal@gmail.com>
2022-04-07 23:20:22 +0530

    Added ch robinson auth for test

--- src/views/ownerOperator/OwnerOperator.js
+++ src/views/ownerOperator/OwnerOperator.js
@@ -1,9 +1,26 @@
-import React, {Fragment} from "react";
+import React, {Fragment, useEffect} from "react";
 import {Button} from "@mui/material";
 import EnhancedTable from "../../components/Atoms/table/Table";
 import {useSelector} from "react-redux";
+import axios from "axios";
 
 const OwnerOperator = () => {
+
+    useEffect(() => {
+        axios({
+            method: 'post',
+            url: 'https://api.navisphere.com/v1/oauth/token',
+            data: {
+                "client_id": "0oah9sno3fT68waXq357",
+                "client_secret": "dExV7FfIod7zNjt_N6lUG44TLTcwvJ-IInImQbrr",
+                "audience": "https://inavisphere.chrobinson.com",
+                "grant_type": "client_credentials"
+            }
+        }).then(res => {
+            console.log(res);
+        });
+    }, [])
+
     const tableConfig = {
         rowCellPadding: 'inherit',
         emptyMessage: 'No Owner Operator Found',
