600e69cf94f0798b12f561f38ca9cee32c3a5a92
jugalTiwari <times.jugal@gmail.com>
2022-07-10 15:35:43 +0530

    ThemeProvider added in index js file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {StyledEngineProvider} from '@mui/material/styles'
+import {ThemeProvider} from "@mui/material/styles";
+import {Provider} from "react-redux";
+import store from "./store";
+import {themeNew} from "./components/layout/ui/Theme";
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    // <StyledEngineProvider injectFirst>
+        <ThemeProvider theme={themeNew}>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </ThemeProvider>
+    // </StyledEngineProvider>
+    , document.getElementById('root'));
