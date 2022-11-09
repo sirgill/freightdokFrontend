aa8bfb571be06f497f1f409d45960ab8acef64f0
Kamesh Rathore <kamesh.kr258@gmail.com>
2022-11-09 23:12:51 +0530

    date filter done

--- src/views/openBoard/NewtrulFilters.js
+++ src/views/openBoard/NewtrulFilters.js
@@ -1,5 +1,5 @@
-import {Grid, Stack, FormControl, Button} from "@mui/material";
-import React, {useCallback, useState} from "react";
+import { Grid, Stack, FormControl, Button } from "@mui/material";
+import React, { useCallback, useState } from "react";
 import moment from "moment";
 import AutoComplete from "../../components/Atoms/form/AutoComplete";
 import Input from "../../components/Atoms/form/Input";
@@ -11,8 +11,8 @@ const radioConfig = {
     defaultValue: 'origin_states[]',
     name: 'origin',
     options: [
-        {label: 'City', value: 'origin_city'},
-        {label: 'State', value: 'origin_states[]'},
+        { label: 'City', value: 'origin_city' },
+        { label: 'State', value: 'origin_states[]' },
     ]
 }
 const radioConfig2 = {
@@ -20,15 +20,15 @@ const radioConfig2 = {
     defaultValue: 'destination_states[]',
     name: 'destination',
     options: [
-        {label: 'City', value: 'destination_city'},
-        {label: 'State', value: 'destination_states[]'},
+        { label: 'City', value: 'destination_city' },
+        { label: 'State', value: 'destination_states[]' },
     ]
 }
 
 const options = [
-    {label: 'Select', value: null},
-    {label: 'Reefer', value: 'Reefer'},
-    {label: 'Dry Van', value: 'Dry Van'},
+    { label: 'Select', value: null },
+    { label: 'Reefer', value: 'Reefer' },
+    { label: 'Dry Van', value: 'Dry Van' },
     // { label: 'Dry Van or Reefer', value: 'Dry Van & Reefer' },
 ]
 
@@ -43,41 +43,41 @@ const serialize = (obj = {}) => {
 
 const getQueryString = (form) => {
     const str = serialize(form);
-    console.log(str);
+    // console.log(str);
     return str
 }
 
-const NewtrulFilters = ({getNewTrulList, pageSize, pageIndex, setParams}) => {
+const NewtrulFilters = ({ getNewTrulList, pageSize, pageIndex, setParams }) => {
     const [form, setForm] = useState({});
 
-    const onChange = ({name, value}) => {
+    const onChange = ({ name, value }) => {
         if (name === 'pickup') {
-            const {startDate, endDate} = value;
+            const { startDate, endDate } = value;
             let obj = {
                 pick_up_start_date: startDate,
                 pick_up_end_date: endDate
             }
-            return setForm({...form, ...obj})
+            return setForm({ ...form, ...obj })
         } else if (name === 'dropOff') {
-            const {startDate, endDate} = value;
+            const { startDate, endDate } = value;
             let obj = {
                 drop_off_start_date: startDate,
                 drop_off_end_date: endDate
             }
-            return setForm({...form, ...obj})
+            return setForm({ ...form, ...obj })
         }
-        setForm({...form, [name]: value});
+        setForm({ ...form, [name]: value });
     }
 
-    const handleRadioChange = useCallback(({name, value}) => {
+    const handleRadioChange = useCallback(({ name, value }) => {
 
-        setForm({...form, [name + 'Radio']: value})
+        setForm({ ...form, [name + 'Radio']: value })
     }, [form])
 
     // console.log(form)
     const onSubmit = (e) => {
         e.preventDefault();
-        const {pick_up_start_date, pick_up_end_date, drop_off_start_date, drop_off_end_date} = form;
+        const dates = ["pick_up_start_date", "pick_up_end_date", "drop_off_start_date", "drop_off_end_date"]
         let obj = {}
         if (form['originRadio']) {
             obj[form['originRadio']] = form.origin || ''
@@ -88,19 +88,21 @@ const NewtrulFilters = ({getNewTrulList, pageSize, pageIndex, setParams}) => {
         if (form['equipments[]']) {
             obj['equipments[]'] = form['equipments[]']
         }
-        if (pick_up_start_date.getTime() !== pick_up_end_date.getTime()) {
-            obj['pick_up_date_range'] = `${moment(pick_up_start_date).format('YYYY-MM-DD')}`
-        }
+
+        dates.forEach(dt => {
+            obj[dt] = moment(form[dt]).format('YYYY-MM-DD')
+        })
         let params = getQueryString(obj);
         setParams(params);
         getNewTrulList(pageSize, pageIndex, params)
     }
 
+
     return (
         <Grid container gap={1} component={'form'} noValidate onSubmit={onSubmit} flexWrap={'wrap'}>
             <Stack>
-                <Input name='origin' label='Origin' onChange={onChange}/>
-                <FormControl sx={{m: 0.5}} variant="standard">
+                <Input name='origin' label='Origin' onChange={onChange} />
+                <FormControl sx={{ m: 0.5 }} variant="standard">
                     <RadioButtonsGroup
                         config={radioConfig}
                         onChange={handleRadioChange}
@@ -108,8 +110,8 @@ const NewtrulFilters = ({getNewTrulList, pageSize, pageIndex, setParams}) => {
                 </FormControl>
             </Stack>
             <Stack>
-                <Input name='destination' label='Destination' onChange={onChange}/>
-                <FormControl sx={{m: 0.5}} variant="standard">
+                <Input name='destination' label='Destination' onChange={onChange} />
+                <FormControl sx={{ m: 0.5 }} variant="standard">
                     <RadioButtonsGroup
                         config={radioConfig2}
                         onChange={handleRadioChange}
@@ -124,14 +126,14 @@ const NewtrulFilters = ({getNewTrulList, pageSize, pageIndex, setParams}) => {
                     size={'small'}
                     onChange={onChange}
                     name='equipments[]'
-                    sx={{minWidth: 200, width: 'inherit'}}
+                    sx={{ minWidth: 200, width: 'inherit' }}
                 />
             </Stack>
             <Stack>
-                <DateRangePicker label={'Pick Up Date Range'} name='pickup' onChange={onChange}/>
+                <DateRangePicker label={'Pick Up Date Range'} name='pickup' onChange={onChange} />
             </Stack>
             <Stack>
-                <DateRangePicker name='dropOff' onChange={onChange} label='Drop Off Date Range'/>
+                <DateRangePicker name='dropOff' onChange={onChange} label='Drop Off Date Range' />
             </Stack>
             <Stack>
                 <Button type={'submit'} variant='contained'>Search</Button>
