8beb0d35f2ee93dff2cc12f77d37872d35a39578
jugalTiwari <times.jugal@gmail.com>
2022-12-15 20:25:22 +0530

    Removed register link

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,10 +1,10 @@
 import React, { Fragment, useState } from 'react';
-import {Link, useHistory} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
 import PropTypes from 'prop-types';
 import { login } from '../../actions/auth';
 import './authcss/LoginRegister.css';
-import Grid from '@material-ui/core/Grid';
+import Grid from '@mui/material/Grid';
 import EmailIcon from '@mui/icons-material/Email';
 import LockOpenIcon from '@mui/icons-material/LockOpen';
 import { makeStyles } from '@material-ui/core/styles';
@@ -12,10 +12,9 @@ import Button from '@mui/material/Button';
 import { Divider, Typography } from '@mui/material';
 import { Form, FormGroup, Input, InputGroup, InputGroupText, InputGroupAddon } from 'reactstrap';
 
-  const useStyles = makeStyles((theme) => ({
+  const useStyles = makeStyles(() => ({
     root: {
       textAlign: "center",
-      width: '445px',
       margin: 'auto',
       background: '#F7FAFC 0% 0% no-repeat padding-box',
       boxShadow: '0px 14px 80px rgba(34, 35, 58, 0.2)',
@@ -36,11 +35,12 @@ import { Form, FormGroup, Input, InputGroup, InputGroupText, InputGroupAddon } f
     },
     gridBottom: {
       padding: '5rem 2rem',
+      width: 445
     }
   }));
   const verticalAlignStyle = { position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)' };
 
-const Login = (props) => {
+const Login = () => {
   const dispatch = useDispatch(),
     history = useHistory();
   const classes = useStyles();
@@ -134,7 +134,7 @@ const Login = (props) => {
                 </Button>
               </Form>
               <br/>
-                <Link to="/entity" className="text-center" underline="none">Register</Link>
+                {/*<Link to="/entity" className="text-center" underline="none">Register</Link>*/}
             </Grid>
           </Grid>
         </div>
@@ -149,9 +149,4 @@ Login.propTypes = {
   isAuthenticated: PropTypes.bool
 
 };
-
-const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
-});
-
 export default (Login);
