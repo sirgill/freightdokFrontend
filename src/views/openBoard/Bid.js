d2c7d0a48e5bdc223b1e8f41ac7db66c9f9ca029
vicky-zluri <vicky.y@zluri.com>
2023-09-17 11:38:05 +0530

    update ch bids

--- src/views/openBoard/Bid.js
+++ src/views/openBoard/Bid.js
@@ -1,15 +1,15 @@
 import Modal from "../ownerOperator/Modal";
 import moment from "moment";
-import {useHistory} from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 import InputField from "../../components/Atoms/form/InputField";
-import React, {useState} from "react";
-import {Button, Grid, Typography, Stack, IconButton} from "@mui/material";
+import React, { useState } from "react";
+import { Button, Grid, Typography, Stack, IconButton } from "@mui/material";
 import AddIcon from '@mui/icons-material/Add';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 import RemoveIcon from '@mui/icons-material/Remove';
-import {placeNewTrulBid, bookNow, newTrulFinalOffer, placeNewTrulCounterOffer} from "../../actions/openBoard.action";
-import {NEWTRUL, productionPayload} from "./constants";
-import {requestPost} from "../../utils/request";
+import { placeNewTrulBid, bookNow, newTrulFinalOffer, placeNewTrulCounterOffer } from "../../actions/openBoard.action";
+import { NEWTRUL, productionPayload } from "./constants";
+import { requestPost } from "../../utils/request";
 
 /*
 * {
@@ -25,8 +25,8 @@ import {requestPost} from "../../utils/request";
 }
 * */
 const saveCHOfferRequestId = async (payload = '', history) => {
-    const {success} = await requestPost({uri: '/api/bid/saveChOfferRequestId', body: payload});
-    if(success){
+    const { success } = await requestPost({ uri: '/api/bid/saveChOfferRequestId', body: payload });
+    if (success) {
         history.goBack();
     }
 }
@@ -34,17 +34,17 @@ const saveCHOfferRequestId = async (payload = '', history) => {
 const Bid = (props) => {
 
     const {
-            location: {state: row = {}} = {},
-            match: {params: {loadNumber: loadNum, counterOffer = false, finalOffer = false}} = {}
-        } = props,
+        location: { state: row = {} } = {},
+        match: { params: { loadNumber: loadNum, counterOffer = false, finalOffer = false } } = {}
+    } = props,
         history = useHistory(),
-        {loadNumber = '', company, vendor, price} = row;
+        { loadNumber = '', company, vendor, price } = row;
     let defaultCost = 0;
     const config = {
         showClose: true
     };
     if (row.hasOwnProperty("availableLoadCosts")) {
-        const {availableLoadCosts = []} = row || {};
+        const { availableLoadCosts = [] } = row || {};
         const [item] = availableLoadCosts || [];
         if (item) {
             defaultCost = item.sourceCostPerUnit || 0
@@ -56,9 +56,29 @@ const Bid = (props) => {
         setAmount(text);
     };
 
-    const afterSubmit = ({success, data}) => {
+    const afterChSubmit = (success, data) => {
+        if (data.offerRequestId) {
+            const payload = {
+                status: false,
+                loadNumber,
+                bidAmount: amount,
+                vendorName: 'C.H. Robinson',
+                loadDetail: row,
+                offerRequestId: data.offerRequestId
+            }
+            saveCHOfferRequestId(payload, history)
+        }
+        else history.goBack();
+
+    }
+
+
+    const afterSubmit = (success, data) => {
+        console.log("+++++++++")
+        console.log(data)
+        console.log("+++++++++")
         if (data?.success || data.status === 'success') {
-            if(data.offerRequestId) {
+            if (data.offerRequestId) {
                 const payload = {
                     status: false,
                     loadNumber,
@@ -116,12 +136,12 @@ const Bid = (props) => {
         });
         const body = {
             "carrierCode": productionPayload.carrierCode,
-            "offerPrice": amount,
+            "offerPrice": parseFloat(amount),
             "offerNote": '',
             "currencyCode": "USD",
-            availableLoadCost: defaultCost
+            // availableLoadCost: defaultCost
         }
-        await bookNow(loadNumber, body, afterSubmit);
+        await bookNow(loadNumber, body, afterChSubmit);
     };
 
     const onSubtract = () => {
@@ -135,17 +155,17 @@ const Bid = (props) => {
 
     return (
         <Modal config={config}>
-            <Grid sx={{px: 3}} justifyContent="center" display="flex">
-                <form onSubmit={onSubmit} style={{textAlign: 'center'}} className={'form_bidding'}>
-                    <Typography sx={{fontSize: 32}}>
+            <Grid sx={{ px: 3 }} justifyContent="center" display="flex">
+                <form onSubmit={onSubmit} style={{ textAlign: 'center' }} className={'form_bidding'}>
+                    <Typography sx={{ fontSize: 32 }}>
                         {company}
                     </Typography>
-                    <Typography sx={{fontSize: 32}}>
+                    <Typography sx={{ fontSize: 32 }}>
                         Load Number: {loadNumber}
                     </Typography>
-                    <Stack direction={'row'} sx={{py: 5}} alignItems={'end'} gap={'10px'} justifyContent={'center'}>
+                    <Stack direction={'row'} sx={{ py: 5 }} alignItems={'end'} gap={'10px'} justifyContent={'center'}>
                         <IconButton onClick={onSubtract} disabled={amount <= 0}>
-                            <RemoveIcon/>
+                            <RemoveIcon />
                         </IconButton>
                         <div className='dollarInput'>
                             <InputField
@@ -158,10 +178,10 @@ const Bid = (props) => {
                             />
                         </div>
                         <IconButton onClick={onAdd}>
-                            <AddIcon/>
+                            <AddIcon />
                         </IconButton>
                     </Stack>
-                    <Button variant="contained" color="success" onClick={onSubmit} sx={{px: 3, py: 1, fontSize: 16}}>
+                    <Button variant="contained" color="success" onClick={onSubmit} sx={{ px: 3, py: 1, fontSize: 16 }}>
                         Submit Bid
                     </Button>
                 </form>
