9d1f708de5a8b5938e15fc7f77c19042532d090e
jugalTiwari <times.jugal@gmail.com>
2023-12-05 20:34:11 +0530

    build

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -60,13 +60,17 @@ Object.defineProperty(String.prototype, 'capitalize', {
 })
 
 function parseToken(token = '') {
-    const base64Url = token.split('.')[1];
-    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+    if(token){
+        const base64Url = token.split('.')[1];
+        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
 
-    return JSON.parse(jsonPayload) || {};
+        return JSON.parse(jsonPayload) || {};
+    } else {
+        return {}
+    }
 }
 
 const getUserDetail = () => {
