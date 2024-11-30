8eb257a6483c013582be94fcd4a1b01ca90413ae
jugalTiwari <times.jugal@gmail.com>
2024-12-01 01:36:03 +0530

    removed consoles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
-import {BrowserRouter, Route, Switch, Redirect} from "react-router-dom";
-import {useDispatch} from "react-redux";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { useDispatch } from "react-redux";
 import PrivateRoute from "./components/routing/PrivateRoute";
 import setAuthToken from "./utils/setAuthToken";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Notification from "./components/layout/Notification";
 
 import "./App.css";
-import {getBaseUrl} from "./config";
+import { getBaseUrl } from "./config";
 import {
     FEDERAL_SIGNUP_LINK,
     FMCSA_VERIFICATION_LINK, FORGOT_PASSWORD,
@@ -16,14 +16,14 @@ import {
     SIGNUP_SUPPORT
 } from "./components/constants";
 import LoadModuleAsync from "./components/Atoms/LoadModuleAsync";
-import {ENHANCED_DASHBOARD} from "./components/client/routes";
-import {Box} from "@mui/material";
-import {buildAppVariables} from "./actions/app.action";
+import { ENHANCED_DASHBOARD } from "./components/client/routes";
+import { Box } from "@mui/material";
+import { buildAppVariables } from "./actions/app.action";
 
 const SignUp = LoadModuleAsync(() => import("./components/auth/signUpWithFMCSA/SignUp"))
 const Support = LoadModuleAsync(() => import("./components/auth/signUpWithFMCSA/Support"))
 const LandingPage = LoadModuleAsync(() => import("./views/landingPage/LandingPage"));
-const UserOnboard = LoadModuleAsync(() => import("./components/auth/signUpWithFMCSA/UserOnboard")) ;
+const UserOnboard = LoadModuleAsync(() => import("./components/auth/signUpWithFMCSA/UserOnboard"));
 const EnhancedDashboard = LoadModuleAsync(() => import("./layout/EnhancedDashboard"), true);
 const Login = LoadModuleAsync(() => import('./components/auth/Login'), true);
 const FMCSASignup = LoadModuleAsync(() => import("./components/auth/signUpWithFMCSA/FMCSASignup"), true)
@@ -38,12 +38,12 @@ axios.defaults.baseURL = getBaseUrl();
 const PreAuthRoutes = () => {
     return <>
         {/*<Route path="/register" component={Register}/>*/}
-        <Route path={LOGIN_LINK} component={Login}/>
-        <Route path={FEDERAL_SIGNUP_LINK} component={SignUp}/>
-        <Route path={FMCSA_VERIFICATION_LINK} component={FMCSASignup}/>
-        <Route path={SIGNUP_SUPPORT} component={Support}/>
-        <Route path={ONBOARDING_USER} component={UserOnboard}/>
-        <Route path={FORGOT_PASSWORD} component={ForgotPassword}/>
+        <Route path={LOGIN_LINK} component={Login} />
+        <Route path={FEDERAL_SIGNUP_LINK} component={SignUp} />
+        <Route path={FMCSA_VERIFICATION_LINK} component={FMCSASignup} />
+        <Route path={SIGNUP_SUPPORT} component={Support} />
+        <Route path={ONBOARDING_USER} component={UserOnboard} />
+        <Route path={FORGOT_PASSWORD} component={ForgotPassword} />
     </>
 }
 
@@ -53,11 +53,10 @@ const App = () => {
     useEffect(() => {
         dispatch(buildAppVariables())
     }, [dispatch])
-    console.log(process.env)
 
     return (
-        <Box sx={{height: '100%'}} className='app'>
-            <Notification/>
+        <Box sx={{ height: '100%' }} className='app'>
+            <Notification />
             <BrowserRouter>
                 <Switch>
                     <Route exact path={'/'}><Redirect to={ENHANCED_DASHBOARD} /></Route>
@@ -73,7 +72,7 @@ const App = () => {
                     {/*<PrivateRoute path="/create-profile" component={ProfileForm}/>*/}
                     {/*<PrivateRoute path="/edit-profile" component={EditProfile}/>*/}
                     {/*<PrivateRoute path="/loads" component={Loads}/>*/}
-                    <PreAuthRoutes/>
+                    <PreAuthRoutes />
                 </Switch>
             </BrowserRouter>
         </Box>
