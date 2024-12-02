9f101f1b671e85729f534138a98a4169debf16f9
jugalTiwari <times.jugal@gmail.com>
2024-12-03 00:32:27 +0530

    build created

---
+++ build/static/js/34.fe26dd88.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpclient=this.webpackJsonpclient||[]).push([[34],{1092:function(e,t,i){"use strict";i.r(t);var n=i(201),a=i(88),c=i(2);t.default=()=>Object(c.jsxs)(n.a,{children:[Object(c.jsx)(a.a,{variant:"h2",align:"center",fontWeight:600,children:"401: Unauthorized"}),Object(c.jsx)(a.a,{align:"center",variant:"h5",children:"You are not authorized to view this page."})]})}}]);
+//# sourceMappingURL=34.fe26dd88.chunk.js.map
